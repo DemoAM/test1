21080bdacd54a0064d8cb85453d24741525bab65
malik <abdulmateennmalik@gmail.com>
2024-06-23 10:22:42 +0500

    removes

--- book/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsAuthor(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_author:
-            return True
--- test_logic01/settings.py
+++ test_logic01/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "book",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -123,4 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = "book.User"
+AUTH_USER_MODEL = "user.User"
